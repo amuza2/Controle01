170ec643811c152de2c457ea11e126e42e65c27e
MohammedChami <mohamuza@gmail.com>
2024-04-29 09:51:42 +0100

    Add project files.

---
+++ ControleDamMedChami/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ControleDamMedChami
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ControleDamMedChami/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ControleDamMedChami
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
